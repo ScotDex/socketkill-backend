653c9fe3e177503ee212d8a1c6cd9d0d01eee7ab
ScottishDex <35346954+ScotDex@users.noreply.github.com>
2026-03-11 11:28:41 +0000

    comment rotation

--- src/services/corpIntelFactory.js
+++ src/services/corpIntelFactory.js
@@ -1,18 +1,29 @@
 const helpers = require('../core/helpers')
+
+const LOSS_COMMENTS = [
+    "Another one bites the dust",
+    "Should have logged off",
+    "Reminder: undocking is optional.",
+    "Somewhere a spy is very pleased.",
+    "Another generous donation to the killboard.",
+    "That was a very loud mistake."
+]
+
 class corpIntelFactory {
     static createKillEmbed(kill, zkb, names) {
 
         const totalValue = helpers.formatIsk(zkb.totalValue)
         const corpIcon = `https://edge.socketkill.com/taylr/logo.png`;
+        const title = LOSS_COMMENTS[Math.floor(Math.random() * KILL_COMMENTS.length)];
         return {
-            username: "Blank Space",
+            username: "Wall of Shame Member",
             avatar_url: corpIcon,
             embeds: [{
                 author: {
                     name: `${names.charName} lost a ${names.shipName}`,
                     icon_url: `https://images.evetech.net/characters/${kill.victim.character_id}/portrait?size=128`
                 },
-                title: `Wall of Shame Member`,
+                title: title,
                 url: `https://zkillboard.com/kill/${kill.killmail_id}/`,
                 thumbnail: { url: `https://images.evetech.net/types/${kill.victim.ship_type_id}/render?size=256` },
                 color: 0xf39c12,
