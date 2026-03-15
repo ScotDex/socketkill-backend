c81c77cd73236515bc07d5206cabb03eb4e14f22
ScotDex <themadlyscientific@gmail.com>
2026-03-15 21:54:41 +0000

    new agent

--- src/network/agent.js
+++ src/network/agent.js
@@ -1,8 +1,8 @@
-const http2wrapper = require('http2-wrapper');
+const http2adapter = require('axios-http2-adapter');
 const axios = require ('axios');
 
 const talker = axios.create({
-    httpsAgent: new http2wrapper.Agent(),
+    adapter: http2adapter,
     timeout: 15000,
     headers:  {
         'Accept-Encoding': 'gzip, deflate, br',
