8335e837f7892237d11467e79e3195cbab1fb15b
ScotDex <themadlyscientific@gmail.com>
2026-04-11 23:14:43 +0100

    adding back end information

--- src/services/webServer.js
+++ src/services/webServer.js
@@ -126,6 +126,7 @@ function startWebServer(esi, statsManager, sharedState, getProcessor) {
       ] = await Promise.all([
         esi.getCharacterName(victim.character_id),
         esi.getCorporationName(victim.corporation_id),
+        victim.alliance_id ? esi.getAllianceName(victim.alliance_id) : Promise.resolve(null),
         esi.getTypeName(victim.ship_type_id),
         esi.getCharacterName(finalBlow.character_id),
         esi.getCorporationName(finalBlow.corporation_id),
@@ -155,6 +156,8 @@ function startWebServer(esi, statsManager, sharedState, getProcessor) {
           characterID: victim.character_id,
           corp: victimCorp,
           corporationID: victim.corporation_id,
+          alliance: victimAlliance,
+          allianceID: victim.alliance_id || null,
           ship: victimShip,
           shipTypeID: victim.ship_type_id,
           damageTaken: victim.damage_taken
