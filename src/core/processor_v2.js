85254d15b7c96c27be5fb8961243c9f4da68834b
ScottishDex <35346954+ScotDex@users.noreply.github.com>
2026-07-17 14:17:50 +0000

    testing repoint

--- src/core/processor_v2.js
+++ src/core/processor_v2.js
@@ -90,7 +90,7 @@ module.exports = (esi, io, statsManager) => {
                 locationLabel: `System: ${systemName} | Region: ${regionName} | Final Blow: ${finalBlowCorp}`,
                 zkillUrl: helpers.getSocketKillLink(killID),
                 victimName: finalVictimName,
-                shipImageUrl: `https://images.evetech.net/types/${killmail.victim.ship_type_id}/render`,
+                shipImageUrl: `https://images.evetech.net/types/${killmail.victim.ship_type_id}/render`, // testing
                 corpImageUrl: `https://api.socketkill.com/render/corp/${killmail.victim.corporation_id}`,
                 allianceImageUrl: `https://api.socketkill.com/render/alliance/${killmail.victim.alliance_id}`,
                 finalBlowCorp: finalBlowCorp,
