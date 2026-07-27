7ff6aaf4cc24b646f32d107d7fdde18e98220d3d
ScottishDex <35346954+ScotDex@users.noreply.github.com>
2026-07-27 17:30:54 +0000

    imports

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const statsManager = require("./src/services/statsManager");
 const ProcessorFactory = require("./src/core/processor_v2");
 const r2 = require("./src/network/r2Writer");
 const hashCache = require("./src/state/hashCache")
-const { syncMarketPrices, loadMarketPrices, startMarketSync } = require("./src/services/priceService");
+const { syncMarketPrices, loadMarketPrices, startMarketSync, getPricedTypeIDs } = require("./src/services/priceService");
 const kv = require('./src/network/kvClient');
 const { startJaniceOverlay, runOverlayPass } = require("./src/services/janiceOverlay");
 
