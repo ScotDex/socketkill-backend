fbf0b4c158b0f53c0a70594d0a76ba22b02a29f3
ScottishDex <35346954+ScotDex@users.noreply.github.com>
2026-08-30 23:48:40 +0100

    patching

--- src/core/itemResolver.js
+++ src/core/itemResolver.js
@@ -50,6 +50,7 @@ function groupForFlag(flag) {
 function flatten(items, out = []) {
     for (const item of items || []) {
         out.push(item);
+        if (item.items?.length) flatten(item.items, out);
     }
     return out;
 }
